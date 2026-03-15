7dafee399ed9abedb53ae9ae90f6ebd618876c7b
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2026-03-15 13:53:26 -0400

    Commit viewer build artifacts for Vercel deployment

---
+++ www/public/viewer/assets/index-F3OG0nQy.js
@@ -0,0 +1,17 @@
+import { invoke as a } from "./core-mPlcS5K-.js";
+async function r(e = {}) {
+  return typeof e == "object" && Object.freeze(e), await a("plugin:dialog|open", { options: e });
+}
+async function o(e = {}) {
+  return typeof e == "object" && Object.freeze(e), await a("plugin:dialog|save", { options: e });
+}
+async function l(e, n) {
+  var _a, _b, _c;
+  const t = typeof n == "string" ? { title: n } : n;
+  return await a("plugin:dialog|ask", { message: e.toString(), title: (_a = t == null ? void 0 : t.title) == null ? void 0 : _a.toString(), kind: t == null ? void 0 : t.kind, yesButtonLabel: (_b = t == null ? void 0 : t.okLabel) == null ? void 0 : _b.toString(), noButtonLabel: (_c = t == null ? void 0 : t.cancelLabel) == null ? void 0 : _c.toString() });
+}
+export {
+  l as ask,
+  r as open,
+  o as save
+};
